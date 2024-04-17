e8f18f3a8940a4cb845838996a1faf679edf766c
ashwininepa <ashwini.visave@nepa.com>
2024-04-17 17:03:19 +0200

    Working with strings

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+string sentence1 = "My name is John";
+string sentence2 = "I like coding";
+Console.WriteLine(sentence1);
+Console.WriteLine(sentence2);
