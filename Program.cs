09f6bc4bb8273c60bd571a6d38ef6d5749822103
ashwininepa <ashwini.visave@nepa.com>
2024-04-17 17:04:53 +0200

    Experiment with string

--- Program.cs
+++ Program.cs
@@ -1,5 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-string sentence1 = "My name is John";
-string sentence2 = "I like coding";
-Console.WriteLine(sentence1);
-Console.WriteLine(sentence2);
+string name = "John";
+Console.WriteLine("My name is " + name);
