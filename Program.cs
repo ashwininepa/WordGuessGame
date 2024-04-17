8d1028f976ebca34d630ccc1c00676ce14e3e82e
ashwininepa <ashwini.visave@nepa.com>
2024-04-17 17:17:17 +0200

    Experiment with numbers and strings

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("My name is " + Console.ReadLine() + ". I am " + Console.ReadLine() + ".");
+string sentence1 = "I enjoy learning";
+string sentence2 = "I like coding";
+Console.WriteLine(sentence1);
+Console.WriteLine(sentence2);
+
+int num1 = 5;
+int num2 = 10;
+int sum = num1 + num2;
+Console.WriteLine(sum);
+
+string name = Console.ReadLine();
+Console.WriteLine("My name is " + name);
+
+string age = Console.ReadLine();
+Console.WriteLine("I am " + age);
