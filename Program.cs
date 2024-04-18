3e2b2a41c0481366c2cd1ec8053401c03c4aae63
ashwininepa <ashwini.visave@nepa.com>
2024-04-18 13:49:40 +0200

    Game is here out now! Previous commit got messed up :D

--- Program.cs
+++ Program.cs
@@ -1,45 +1,37 @@
-﻿// // Console game to guess a word from a list of words
+﻿// Console game to guess a word from a list of words
 
-// Random rand = new Random();
-// string[] words = ["dog", "cat", "boy", "girl"];
-// string wordToGuess = words[rand.Next(0, words.Length)];
+Random rand = new Random();
+string[] words = ["dog", "cat", "boy", "girl"];
+string wordToGuess = words[rand.Next(0, words.Length)];
 
-// bool hasWon = false;
+bool hasWon = false;
 
-// for(int i = 0; i < words.Length; i++)
-// {
-//     Console.WriteLine("Guess the word: ");
-
-//     for(int j = 0; j < wordToGuess.Length; j++)
-//     {
-//         Console.WriteLine(words[j] + " ");
-//     }
-    
-//     Console.WriteLine();
-
-//     Console.WriteLine("Enter your guess: ");
-//     string guess = Console.ReadLine();
-
-//     if(guess == wordToGuess)
-//     {
-//         hasWon = true;
-//         break;
-//     }
-// }
-
-// if(hasWon)
-// {
-//     Console.WriteLine("You have won!");
-// }
-// else
-// {
-//     Console.WriteLine("You have lost! The word was: " + wordToGuess);
-// }
-
-for(int i = 0; i < 5; i++)
-
+for(int i = 0; i < words.Length; i++)
 {
+    Console.WriteLine("Guess the word: ");
 
-    Console.Write(i + "");
+    for(int j = 0; j < wordToGuess.Length; j++)
+    {
+        Console.WriteLine(words[j] + " ");
+    }
+    
+    Console.WriteLine();
 
+    Console.WriteLine("Enter your guess: ");
+    string guess = Console.ReadLine();
+
+    if(guess == wordToGuess)
+    {
+        hasWon = true;
+        break;
+    }
+}
+
+if(hasWon)
+{
+    Console.WriteLine("You have won!");
+}
+else
+{
+    Console.WriteLine("You have lost! The word was: " + wordToGuess);
 }
